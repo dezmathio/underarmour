7a22eeae8ee0b8f0ecc8b180007c5596e738fa6f
@dezmathio <janjos@weddingwire.com>
2017-10-14 19:28:53 -0400

    privacy_option scaffolding

--- spec/underarmour_spec.rb
+++ spec/underarmour_spec.rb
@@ -14,10 +14,22 @@ RSpec.describe Underarmour do
     let(:access_token) { 'access_token_returned_from_ua' }
     let(:api_key) {'client_id_provided_by_ua'}
 
-    it 'returns a user' do
-      StubApi::User.find_self
-      user = Underarmour::ApiClient.scoped_api_client(Underarmour::User, 'user/', access_token: access_token, api_key: api_key).find_self
-      expect(user.first_name).to eq 'Oswald'
+    context '#user' do
+      it 'returns a user' do
+        StubApi::User.find_self
+        user = Underarmour::ApiClient.scoped_api_client(Underarmour::User, 'user/', access_token: access_token, api_key: api_key).find_self
+        expect(user.first_name).to eq 'Oswald'
+      end
+    end
+    context '#privacy_option' do
+      
+      xit 'returns a privacy option description' do
+
+      end
+
+      xit 'returns the privacy options total_count' do
+
+      end
     end
   end
 end
